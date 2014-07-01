bb7d0911c78390742189254e72bcb613267485d0
Thierry Stiegler <thierry.stiegler@gmail.com>
2014-07-01 11:39:07 +0200

    bash aliases to put in your .bash_alias file

---
+++ dockertools_alias.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+alias docker.io='sudo docker.io'
+alias dkenter='sudo /home/docker/bin/dkenter.sh'
+alias dkclean='/home/docker/bin/dkclean.sh'
+alias dkcleancontainers='/home/docker/bin/dkcleancontainers.sh'
+alias dkcleanimages='/home/docker/bin/dkcleanimages.sh'
+alias pipework='sudo /home/docker/bin/pipework'
